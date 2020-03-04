5c9780f95ca59abd4ee99d9a1c3b9040004e32f7
NiuRuslan <niuruslan@gmail.com>
2020-03-04 18:19:22 +0300

    release null

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>SOON</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
