9f99680c995792e3c239a0aec869a8986a2b2ac0
Aleksandr Ivanov <aleksand7@yandex.ru>
2020-03-05 12:22:25 +0300

    route users/login done

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -10,6 +10,40 @@ router.get('/', async function (req, res, next) {
 
 /**
  *  Aleksandr Ivanov
+ * Проверяю данные пользователя: 
+ * @email
+ * @password
+ * Отдаю объект:
+ * @success - флаг выполнения запроса
+ * @data - объект с данными пользователя
+ *    @nickname
+ *    @id
+ * @err - Расшифровка ошибки
+ */
+router.post('/login', async (req, res) => {
+  const {
+    email,
+    password,
+  } = req.body;
+  const user = await Person.findOne({ email, password });
+  if (user) {
+    return res.send({
+      success: true,
+      date: {
+        nickname: user.nickname, 
+        id: user._id,
+      }
+    });
+  } else {
+    return res.send({
+      success: false,
+      err: 'No such user or incorrect pair login password'
+    })
+  }
+});
+
+/**
+ *  Aleksandr Ivanov
  * Проверяю на уникальность поле: email
  * Вношу нового пользователя в базу
  * Отдаю объект:
