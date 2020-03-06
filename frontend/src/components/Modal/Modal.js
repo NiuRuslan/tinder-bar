4d51d88a51f327d569dea6540bf4d1a21214e7f0
Kiba Olga <olgadamakibovna@gmail.com>
2020-03-06 15:06:37 +0300

    art-4

---
+++ frontend/src/components/Modal/Modal.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+import './modal.css';
+
+
+function Modal() {
+  // const [profiles, setProfiles] = useState([]);
+const profile = props.profile
+  return (
+    <>
+    <ul className="profile">
+  <img src={`${profile.avatar}.jpg`}></img>
+  <h1>Name:{profile.name}</h1>
+  <li className="list">Date of Birth:{profile.DoB}</li>
+  <li className="list">Activity:{profile.activity}</li>
+  <li className="list">Favotite drinks:{profile.drinks}</li>
+  <li className="list">Favotite topics:{profile.topics}</li>
+  <li className="list">About yourself:{profile.about}</li>
+  </ul>
+</>
+  );
+}
+
+export default Modal
--- frontend/src/components/slider/Slider2.js
+++ frontend/src/components/slider/Slider2.js
@@ -8,16 +8,7 @@ function Slider2() {
    
 
 <div className="slideshow">
-  {/* <img className="slideshow-image" src="/imgs/elevate-KJzrLIfq2Zo-unsplash.jpg"></img>
-  <img className="slideshow-image" src="/imgs/elevate-UoPNA8I-_p0-unsplash.jpg"></img>
-
-  <img className="slideshow-image" src="/imgs/jinsoo-choi-Ydp9jXqr1cI-unsplash.jpg"></img>
-
-  <img className="slideshow-image" src="/imgs/john-arano-_qADvinJi20-unsplash.jpg"></img>
-
-  <img className="slideshow-image" src="/imgs/john-fornander-SiKzrE3ywSc-unsplash.jpg"></img> */}
-
-
+ 
   <div className="slideshow-image" style={{backgroundImage: "url('./imgs/10.jpg')"}}></div>
   <div className="slideshow-image" style={{backgroundImage: "url('./imgs/11.jpg')"}}></div>
   <div className="slideshow-image" style={{backgroundImage: "url('./imgs/3.jpg')"}}></div>
