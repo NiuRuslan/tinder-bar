5e195c388cc339e61e94bbe767d2aeef3515f56d
Daniil <Danyant@mail.ru>
2020-03-11 11:12:11 +0200

    Finish chat

--- backend/routes/database.js
+++ backend/routes/database.js
@@ -10,17 +10,16 @@ getChatName = (a, b) => {
   }
 }
 
-router.post('/', (req,res)=>{
+router.post('/',async (req,res)=>{
 const {ID1, ID2} = req.body;
 const chat = getChatName(ID1,ID2)
-console.log(chat)
-const user =  Person.findOne({_id:ID1})
-const user2 = Person.findOne({_id:ID2})
+const user = await Person.findOne({_id:ID1})
+const user2 = await Person.findOne({_id:ID2})
 if(user.chats.includes(chat) && user2.chats.includes(chat)){
 return res.send()
 } else {
-   Person.updateOne({_id:ID1},{$push: { chats:chat }})
-   Person.updateOne({_id:ID2},{$push: { chats:chat }})
+  await Person.updateOne({_id:ID1},{$push: { chats:chat }})
+  await Person.updateOne({_id:ID2},{$push: { chats:chat }})
   res.send()
 }
 })
