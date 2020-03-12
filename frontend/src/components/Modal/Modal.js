0dcd53b3bf5797e4b974c3fb5834e2c4a54e586e
NiuRuslan <niuruslan@gmail.com>
2020-03-12 16:04:15 +0300

    modals is done

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './modal.css';
-import {
-  Button, Header, Modal, List,
-} from 'semantic-ui-react';
 import axios from 'axios';
+import {
+  Button, Header, Modal, List, Card
+} from 'semantic-ui-react';
+import './modal.css';
 import { useCookies } from 'react-cookie';
 import { Link } from 'react-router-dom';
 
@@ -28,7 +28,7 @@ function ModalWindow(props) {
     <div>
       <Modal
         style={{
-          textAlign: 'center',
+          textAlign: 'center',  height: 'auto'
         }}
         dimmer="blurring"
         size="mini"
@@ -39,31 +39,39 @@ function ModalWindow(props) {
               listStyle: 'none',
               alignSelf: 'center',
               position: 'relative',
-              border: 'solid 2px #E74C3C',
-              borderRadius: '8px',
               backgroundColor: 'transparent',
               flex: '1',
               color: '#FFF',
             }}
           >
-            {profile.name}
+            <Card style={{ backgroundColor: 'transparent', border: 'solid 2px #E74C3C', borderRadius: '8px', width: '200px' }}>
+              <Card.Content>
+                <div className="mini" style={{ backgroundImage: `url(${profile.url || './imgs/info.png'})` }} />
+                <Card.Header textAlign='center'></Card.Header>
+                <Card.Description style={{ color: 'white' }}>
+                  {profile.name}, {age}
+                </Card.Description>
+              </Card.Content>
+            </Card>
           </Button>
+
         )}
       >
-        <Modal.Header style={{ backgroundColor: '#0f4667' }}></Modal.Header>
         <Modal.Content>
           <Modal.Description style={{ color: '#0f4667' }}>
-            <Header style={{ color: '#0f4667' }}>
+            <Header style={{ color: '#0f4667', fontSize: 'x-large' }}>
               {` ${profile.name}, ${age}`}
             </Header>
             <div className="avatar cursor" style={{ backgroundImage: `url(${profile.url || './imgs/info.png'})` }} />
-            <List><List.Item icon="briefcase" content={profile.activity} /></List>
-            <List><List.Item icon="glass martini" content={profile.drinks} /></List>
-            <List><List.Item icon="comments" content={profile.topics} /></List>
-            <List><List.Item icon="info circle" content={profile.about} /></List>
+            <List style={{ padding: '0 3rem', fontSize: 'large' }}>
+              <List.Item icon="briefcase" content={profile.activity} />
+              <List.Item icon="glass martini" content={profile.drinks} />
+              <List.Item icon="comments" content={profile.topics} />
+              <List.Item icon="info circle" content={profile.about} />
+            </List>
           </Modal.Description>
         </Modal.Content>
-        <Modal.Actions style={{ backgroundColor: '#0f4667' }}>
+        <Modal.Actions style={{ backgroundColor: '#0f4667', textAlign: 'center' }}>
           <Link
             onClick={sendRequest}
             to={{
@@ -80,7 +88,7 @@ function ModalWindow(props) {
               style={{
                 color: '#0f4667',
                 textShadow: 'none',
-                margin: '0 auto',
+                marginBottom: '1em',
                 borderRadius: '320px',
                 backgroundColor: '#FFF',
               }}
