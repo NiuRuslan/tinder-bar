fa38441343fec5cc8969bec818da5e43cf217d62
Kiba Olga <olgadamakibovna@gmail.com>
2020-03-13 17:08:08 +0300

    last5

--- frontend/src/components/Announcement/Announcement.js
+++ frontend/src/components/Announcement/Announcement.js
@@ -22,6 +22,8 @@ function AnnouncementMessage(props) {
   const deleteAnnoun = () => {
     setMan(null);
   };
+  console.log(getChatName(user.friend, cookies.userName));
+  console.log(user.friend, cookies.userName);
 
   return (
     <>
@@ -43,7 +45,8 @@ function AnnouncementMessage(props) {
                 chats: getChatName(cookies.userName, user.friend),
                 name: user.name,
                 urlFriend: user.url,
-                date: user.date
+                friend: user.friend,
+                url: ""
               }
             }}
           >
